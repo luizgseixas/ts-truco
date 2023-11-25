e1ff233a6a02addd043b2f7dc109503f69af851a
Luiz Seixas <ticoluizgustavo@outlook.com>
2023-11-25 01:35:16 -0300

    style: remove unnecessary code

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,5 @@
 // nipes p paus, c copas, e espadas, o ouros
-let manilhas = ['4z', '7c', 'ae', '7o']
-
-// enum Carts {
-//   'Ao' = 11,
-//   '2o' = 12,
-//   '3o' = 13,
-//   '4o' = 14,
-//   '5o' = 15,
-//   '6o' = 16,
-//   '7o' = 17,
-//   'Qo' = 18,
-//   'Jo' = 19,
-//   'Ko' = 20,
-//   'Ac' = 21,
-//   '2c' = 22,
-//   '3c' = 23,
-//   '4c' = 24,
-//   '5c' = 25,
-//   '6c' = 26,
-//   '7c' = 27,
-//   'Qc' = 28,
-//   'Jc' = 29,
-//   'Kc' = 30,
-//   'Ae' = 31,
-//   '2e' = 32,
-//   '3e' = 33,
-//   '4e' = 34,
-//   '5e' = 35,
-//   '6e' = 36,
-//   '7e' = 37,
-//   'Qe' = 38,
-//   'Je' = 39,
-//   'Ke' = 40,
-//   'Az' = 41,
-//   '2z' = 42,
-//   '3z' = 43,
-//   '4z' = 44,
-//   '5z' = 45,
-//   '6z' = 46,
-//   '7z' = 47,
-//   'Qz' = 48,
-//   'Jz' = 49,
-//   'Kz' = 50,
-// }
+let manilhas = ['4z', '7c', 'Ae', '7o']
 
 let baralho = [
   'Ao', '2o', '3o', '4o', '5o', '6o', '7o', 'Qo', 'Jo', 'Ko',
@@ -84,5 +41,5 @@ function findDuplicatedCards() {
   return sortedCards.filter((e, i, a) => a.indexOf(e) !== i) 
 }
 
-console.log(findDuplicatedCards());
-console.log({ sortedCards, player1, player2, player3,player4 });
+// console.log(findDuplicatedCards());
+console.log({player1, player2, player3,player4 });
